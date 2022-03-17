7b074182c8647368e4dfa1e61adcc729bd8c2ac3
Pierluigi Viti <pierluigi@commercelayer.io>
2022-03-17 17:10:32 +0100

    chore(test): refactor some specs

--- specs/resource.spec.ts
+++ specs/resource.spec.ts
@@ -87,4 +87,33 @@ describe('SDK:resource suite', () => {
 		expect(customer.id).toEqual(id)
 	})
 
+
+	/*
+	it('resource.relationship.delete', async () => {
+
+		const customerOptions = { fields: ['email', 'customer_group'], include: ['customer_group'] }
+
+		const customers = await cl.customers.list({ pageSize: 1, ...customerOptions })
+		const customer = customers[0]
+		expect(customer.id).not.toBeNull()
+
+		const customerGroups = await cl.customer_groups.list({ pageSize: 1, fields: ['name'] })
+		const customerGroup = customerGroups[0]
+		expect(customerGroup.id).not.toBeNull()
+
+		let c = await cl.customers.update({
+			id: customer.id,
+			customer_group: cl.customer_groups.relationship(customerGroup)
+		}, customerOptions)
+		expect(c.customer_group.id).toEqual(customerGroup.id)
+
+		c = await cl.customers.update({
+			id: customer.id,
+			customer_group: cl.customer_groups.relationship(null)
+		}, customerOptions)
+		expect(c.customer_group).toBeNull()
+
+	})
+	*/
+
 })
