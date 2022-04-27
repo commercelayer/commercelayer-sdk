795ad5dfca6fa521f454f99def6eee3fbaf94b9c
Pierluigi Viti <pierluigi@commercelayer.io>
2022-04-27 13:17:59 +0200

    fix(core): fix error details type

--- src/client.ts
+++ src/client.ts
@@ -27,7 +27,7 @@ const handleError = (error: Error) => {
 			apiError.type = ErrorType.RESPONSE
 			apiError.status = error.response.status
 			apiError.code = String(apiError.status)
-			apiError.errors = error.response.data.errors
+			apiError.errors = error.response.data.errors as any[]
 			sdkError = apiError
 		} else if (error.request) {
 			// The request was made but no response was received
