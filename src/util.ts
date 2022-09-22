2e58cf2dbc8e37e101fd8efd29997566312eb7f6
Pierluigi Viti <pierluigi@commercelayer.io>
2022-09-22 11:55:33 +0200

    chore(core): add util function to sort object fields

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ObjectType = { [key: string]: any }
+
 
 const sleep = async (ms: number): Promise<NodeJS.Timeout> => {
 	return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 
-export { sleep }
+const sortObjectFields = (obj: ObjectType): ObjectType => {
+	const sorted = Object.keys(obj).sort().reduce((accumulator: ObjectType, key: string) => {
+		accumulator[key] = obj[key];
+		return accumulator;
+	}, {})
+	return sorted
+}
+
+
+export { sleep, sortObjectFields }
