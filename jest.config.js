1c84becb682eb35e1f4d4ae052cd1fb7e799cc61
Pierluigi Viti <pierluigi@commercelayer.io>
2022-09-22 12:14:22 +0200

    chore(core): update settings and clean dependencies

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = async () => {
   return {
     verbose: true,
     testMatch: ['**/specs/**/*.spec.[jt]s?(x)'],
-    collectCoverageFrom: ["src/**/{!(ignore-me),}.ts"]
+    collectCoverageFrom: ['src/**/{!(ignore-me),}.ts']
   }
 }
