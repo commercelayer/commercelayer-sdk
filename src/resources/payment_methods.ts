f886c28c47448fdf51dc61661d6a7cc4c3cd66e8
Pierluigi Viti <pierluigi@commercelayer.io>
2023-10-18 12:07:35 +0200

    fix: payment_source_type enum

--- src/resources/payment_methods.ts
+++ src/resources/payment_methods.ts
@@ -19,7 +19,7 @@ interface PaymentMethod extends Resource {
 	readonly type: PaymentMethodType
 
 	name?: string | null
-	payment_source_type: 'adyen_payment' | 'axerve_payment' | 'braintree_payment' | 'checkout_com_payment' | 'credit_card' | 'external_payment' | 'klarna_payment' | 'paypal_payment' | 'satispay_payment' | 'stripe_payment' | 'wire_transfer'
+	payment_source_type: 'adyen_payments' | 'axerve_payments' | 'braintree_payments' | 'checkout_com_payments' | 'credit_cards' | 'external_payments' | 'klarna_payments' | 'paypal_payments' | 'satispay_payments' | 'stripe_payments' | 'wire_transfers'
 	currency_code?: string | null
 	moto?: boolean | null
 	require_capture?: boolean | null
@@ -42,7 +42,7 @@ interface PaymentMethod extends Resource {
 
 interface PaymentMethodCreate extends ResourceCreate {
 	
-	payment_source_type: 'adyen_payment' | 'axerve_payment' | 'braintree_payment' | 'checkout_com_payment' | 'credit_card' | 'external_payment' | 'klarna_payment' | 'paypal_payment' | 'satispay_payment' | 'stripe_payment' | 'wire_transfer'
+	payment_source_type: 'adyen_payments' | 'axerve_payments' | 'braintree_payments' | 'checkout_com_payments' | 'credit_cards' | 'external_payments' | 'klarna_payments' | 'paypal_payments' | 'satispay_payments' | 'stripe_payments' | 'wire_transfers'
 	currency_code?: string | null
 	moto?: boolean | null
 	require_capture?: boolean | null
@@ -58,7 +58,7 @@ interface PaymentMethodCreate extends ResourceCreate {
 
 interface PaymentMethodUpdate extends ResourceUpdate {
 	
-	payment_source_type?: 'adyen_payment' | 'axerve_payment' | 'braintree_payment' | 'checkout_com_payment' | 'credit_card' | 'external_payment' | 'klarna_payment' | 'paypal_payment' | 'satispay_payment' | 'stripe_payment' | 'wire_transfer' | null
+	payment_source_type?: 'adyen_payments' | 'axerve_payments' | 'braintree_payments' | 'checkout_com_payments' | 'credit_cards' | 'external_payments' | 'klarna_payments' | 'paypal_payments' | 'satispay_payments' | 'stripe_payments' | 'wire_transfers' | null
 	currency_code?: string | null
 	moto?: boolean | null
 	require_capture?: boolean | null
