7ab4e98c604aeb74162367fc8b79c20944d72b2a
Pierluigi Viti <pierluigi@commercelayer.io>
2024-09-13 17:47:06 +0200

    chore: modify comment

--- src/commercelayer.ts
+++ src/commercelayer.ts
@@ -166,7 +166,7 @@ class CommerceLayerClient {
 
 		debug('new commercelayer instance %O', config)
 
-		// Take organization and domain from access token if not user defined
+		// Take organization and domain from access token if not defined by user
 		if ((!config.organization || !config.domain) && config.accessToken) {
 			const tokenData = extractTokenData(config.accessToken)
 			if (!config.organization && tokenData?.organization?.slug) config.organization = tokenData.organization.slug
